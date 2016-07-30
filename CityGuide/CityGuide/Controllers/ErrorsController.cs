41a5231d2eb459df66687c76ceb5d160fbf980db
McINTYRE12 <claudiukiss@yahoo.es>
2016-07-30 23:48:10 +0300

    added tours index

---
+++ CityGuide/CityGuide/Controllers/ErrorsController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace CityGuide.Controllers
+{
+    public class ErrorsController : Controller
+    {
+        public ActionResult Error404()
+        {
+            return View();
+        }
+    }
+}
